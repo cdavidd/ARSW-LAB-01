a4be5ed73a914ffb33e3846489b09131591029b1
cristian lopez <cristian.lopez-a@mail.escuelaing.edu.co>
2019-08-14 08:07:36 -0500

    parte 1 lista

--- BBP_FORMULA/PARALLELISM-JAVA_THREADS_MAVEN-INTRODUCTION_BBP_FORMULA/src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ BBP_FORMULA/PARALLELISM-JAVA_THREADS_MAVEN-INTRODUCTION_BBP_FORMULA/src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,12 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+        Thread t1= new CountThread(0,99);
+        Thread t2= new CountThread(100,199);
+        Thread t3= new CountThread(200,299);
+        t1.run();
+        t2.run();
+        t3.run();
     }
     
 }
