a4d007e94609a21533074d3f47cd5142fbc21d74
david <cdavidlopez@hotmail.com>
2019-08-15 19:49:53 -0500

    P2 parte1 50%

--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,59 @@
 package edu.eci.arsw.primefinder;
 
-public class Main {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
-	}
-	
+public class Main extends JFrame implements KeyListener {
+    PrimeFinderThread pft1;
+    PrimeFinderThread pft2;
+    PrimeFinderThread pft3;
+    
+    public static void main(String[] args){
+        /*PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+        pft.start();
+        */
+        Main tes= new Main();
+        tes.inicializador();
+        
+        
+    }
+    public Main(){
+        pft1=new PrimeFinderThread(0, 10000000);
+        pft2=new PrimeFinderThread(10000001, 20000000);
+        pft3=new PrimeFinderThread(20000001, 30000000);
+        pft1.start();
+        pft2.start();
+        pft3.start();
+    }
+    
+    private void inicializador() {
+        setLayout(null);
+        setTitle("Prueba con Teclas");
+        setVisible(true);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        addKeyListener(this);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_ENTER) {
+            pft1.suspend();
+            pft2.suspend();
+            pft3.suspend();
+        }
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }    
 }
