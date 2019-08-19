911b6209b642e993d68a74d9f566a7b9c7a7b8f3
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-08-19 16:12:07 -0500

    PrimeThread test

--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/test/java/edu/eci/arsw/primefinder/PrimeTest.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/test/java/edu/eci/arsw/primefinder/PrimeTest.java
@@ -5,17 +5,80 @@
  */
 package edu.eci.arsw.primefinder;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  *
  * @author cdavi_000
  */
 public class PrimeTest {
-    public PrimeTest(){}
+
+    private PrimeFinderThread pfOne;
+
+    public PrimeTest(){
+
+    }
+
+    @Before
+    public void setup(){
+        pfOne = new PrimeFinderThread(0, 100000);
+        pfOne.start();
+        try {
+            pfOne.join();
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
     
     @Test
-    public void a(){
-    
-    }        
+    public void evenNumberOfThreads() {
+        PrimeFinderThread pfTwo = new PrimeFinderThread(0, 50000);
+        PrimeFinderThread pfThree = new PrimeFinderThread(50001, 100000);
+        pfTwo.start(); pfThree.start();
+        List<Integer> respuesta = new LinkedList<>();
+        try {
+            pfTwo.join();
+            pfThree.join();
+        } catch (InterruptedException e) {
+            fail();
+        }
+        respuesta.addAll(pfTwo.getPrimes());
+        respuesta.addAll(pfThree.getPrimes());
+        assertEquals(respuesta.size() , pfOne.getPrimes().size());
+        List<Integer> comparar = pfOne.getPrimes();
+        for (int i = 0 ; i < respuesta.size(); i++){
+            assertEquals(respuesta.get(i),comparar.get(i));
+        }
+    }
+
+    @Test
+    public void oddNumberOfThreads() {
+        PrimeFinderThread pfTwo = new PrimeFinderThread(0, 33333);
+        PrimeFinderThread pfThree = new PrimeFinderThread(33334, 66666);
+        PrimeFinderThread pfFour = new PrimeFinderThread(66667, 100000);
+        pfTwo.start(); pfThree.start(); pfFour.start();
+        List<Integer> respuesta = new LinkedList<>();
+        try {
+            pfTwo.join();
+            pfThree.join();
+            pfFour.join();
+        } catch (InterruptedException e) {
+            fail();
+        }
+        respuesta.addAll(pfTwo.getPrimes());
+        respuesta.addAll(pfThree.getPrimes());
+        respuesta.addAll(pfFour.getPrimes());
+        assertEquals(respuesta.size() , pfOne.getPrimes().size());
+        List<Integer> comparar = pfOne.getPrimes();
+        for (int i = 0 ; i < respuesta.size(); i++){
+            assertEquals(respuesta.get(i),comparar.get(i));
+        }
+    }
+
 }
